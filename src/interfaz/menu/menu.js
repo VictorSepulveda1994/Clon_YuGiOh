89c7b84558ebd7b83e60de1b7063ba942d3a6986
Dark7Freya <carias14@alumnos.utalca.cl>
2021-04-27 20:14:04 -0300

    vector

--- src/interfaz/menu/menu.js
+++ src/interfaz/menu/menu.js
@@ -1,3 +1,6 @@
+/*  recuperado de:
+*   https://phaser.io/examples/v3/view/input/zones/basic-input-zone#
+*/
 class Menu extends Phaser.Scene
 {
     preload ()
@@ -14,9 +17,6 @@ class Menu extends Phaser.Scene
 
     create ()
     {
-
-        
-
         //Particulas del fondo
         for (var i = 0; i < 300; i++)
         {
@@ -46,7 +46,7 @@ class Menu extends Phaser.Scene
         var punteroIcon = this.add.image(1024, 600, 'puntero').setOrigin(0.5, 0).setAngle(-20).setScale(0.2);
         var label = this.add.text(0, 0, '', { font: "24px Arial Black", fill: "#c51b7d" });
         label.setStroke('#de77ae', 8);
-        
+
         this.input.on('gameobjectdown', function (pointer, gameObject) {
 
             punteroIcon.x = pointer.x;
@@ -57,39 +57,7 @@ class Menu extends Phaser.Scene
             label.y = gameObject.y;
 
         });
-        /*  Mmm, donuts
-        this.add.image(0, 0, 'donuts').setOrigin(0);
-
-        //  A little fork sprite
-        var fork = this.add.image(1024, 600, 'fork').setOrigin(0.5, 0).setAngle(-20);
-
-        var label = this.add.text(0, 0, '', { font: "48px Arial Black", fill: "#c51b7d" });
-        label.setStroke('#de77ae', 8);
-
-        //  And here's the real content.. some hit zones.
-        //  One for each donut in the picture.
         
-        var zone1 = this.add.zone(0, 0, 345, 300).setOrigin(0).setName('Plain').setInteractive();
-        var zone2 = this.add.zone(345, 0, 310, 300).setOrigin(0).setName('Chocolate').setInteractive();
-        var zone3 = this.add.zone(655, 0, 369, 300).setOrigin(0).setName('Coffee\nand cream').setInteractive();
-
-        var zone4 = this.add.zone(0, 300, 330, 300).setOrigin(0).setName('Chocolate\nSprinkles').setInteractive();
-        var zone5 = this.add.zone(330, 300, 350, 300).setOrigin(0).setName('Strawberry').setInteractive();
-        var zone6 = this.add.zone(680, 300, 344, 300).setOrigin(0).setName('More\nSprinkles').setInteractive();
-
-        //  And some events
-
-        this.input.on('gameobjectdown', function (pointer, gameObject) {
-
-            fork.x = pointer.x;
-            fork.y = pointer.y;
-
-            label.setText(gameObject.name);
-            label.x = gameObject.x;
-            label.y = gameObject.y;
-
-        });
-        */
     }
 
     update()
