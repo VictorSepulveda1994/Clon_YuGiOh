30166aed675cead11f3bfe936e17e08dae7dc9b7
Dark7Freya <carias14@alumnos.utalca.cl>
2021-04-27 00:35:41 -0300

    2

--- src/init.js
+++ src/init.js
@@ -3,15 +3,16 @@ var configuracion =
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    parent: 'container',
-    scale: {
+    parent: "container",
+    scale:
+    {
         mode: Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-      },
+        autoCenter: Phaser.Scale.Center
+    },
 };
 
 
 var game = new Phaser.Game(configuracion);
-game.scene.add('presentacion', Presentacion);
+game.scene.add('Presentacion', Presentacion);
 game.scene.add('menu', Menu);
 game.scene.start('presentacion');
