68ffd719b9771623b2ff035f2760e5737ad3d2c7
MaxiViveros <max.antonio.viveros@gmail.com>
2021-04-27 21:23:00 -0400

    avance boton click

---
+++ src/Mazo.js
@@ -0,0 +1,30 @@
+class Mazo extends Phaser.Scene {
+    constructor() {
+        super({key: "Mazo"});
+        var button;
+        var background;
+    }
+
+    preload() {
+
+    }
+
+
+    create() {
+        let graphics = this.add.graphics();
+        graphics.fillStyle(0xff3300, 1);
+        graphics.fillRect(10, 10, 1180, 580);
+        //this.add.text(50, 50, "Menu principal Yu Gi Oh", { font: "50px Courier", fill: "#000000"});
+        //this.add.text(50, 200, "Opcion1", { font: "50px Courier", fill: "#000000"});
+        //this.add.text(50, 300, "Opcion2", { font: "50px Courier", fill: "#000000"});
+        //this.add.text(50, 400, "Opcion3", { font: "50px Courier", fill: "#000000"});
+
+
+    }
+
+
+
+    update(time, delta) {
+
+    }
+}
--- src/init.js
+++ src/init.js
@@ -2,7 +2,9 @@ const config = {
     width: 1200,
     height: 600,
     parent: "contenedor",
-    type: Phaser.AUTO
+    type: Phaser.CANVAS,
+    backgroundColor: '#392542',
+    scene: [Mazo, CrearMazo, EditarMazo]
 }
 
 var game = new Phaser.Game(config);
